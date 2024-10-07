e6585e99fc69083d3bc8be70c229afbc50898f21
vl4d09 <ungureanu.vlad19@gmail.com>
2024-10-07 12:56:11 +0300

    cleaning

--- SRP/src/EmailService.java
+++
@@ -1,7 +0,0 @@
-public class EmailService {
-    public void sendEmail(User user, String message) {
-        // Simulate sending an email
-        System.out.println("Sending email to " + user.getEmail() + ": " + message);
-    }
-}
-
--- SRP/src/UserRepository.java
+++
@@ -1,6 +0,0 @@
-public class UserRepository {
-    public void save(User user) {
-        System.out.println("Saving " + user.getName() + " to the database.");
-    }
-}
-
